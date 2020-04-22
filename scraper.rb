f855e534fe2f77b686109ee194e92180663ca351
yourneighbourhood <yourneighbourhood@users.noreply.github.com>
2020-04-22 21:41:58 +1000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,67 +1,102 @@
-require 'scraperwiki'
-require 'mechanize'
+<?php
+require_once 'vendor/autoload.php';
+require_once 'vendor/openaustralia/scraperwiki/scraperwiki.php';
 
-starting_url = 'https://da.bundaberg.qld.gov.au/modules/ApplicationMaster/default.aspx?page=found&1=thisweek&4a=333,322,321,324,323,325&6=F'
-comment_url = 'mailto:CEO@bundaberg.qld.gov.au'
+use PGuardiario\PGBrowser;
+use Sunra\PhpSimple\HtmlDomParser;
 
-def clean_whitespace(a)
-  a.gsub("\r", ' ').gsub("\n", ' ').squeeze(" ").strip
-end
+date_default_timezone_set('Australia/Sydney');
 
-def scrape_table(doc, comment_url)
-  doc.search('table tbody tr').each do |tr|
-    # Columns in table
-    # Show  Number  Submitted  Details
-    tds = tr.search('td')
+# Default to 'thisweek', use MORPH_PERIOD to change to 'thismonth' or 'lastmonth' for data recovery
+switch(getenv('MORPH_PERIOD')) {
+    case 'thismonth' :
+        $period = 'thismonth';
+        break;
+    case 'lastmonth' :
+        $period = 'lastmonth';
+        break;
+    default         :
+        $period = 'thisweek';
+        break;
+}
+print "Getting data for `" .$period. "`, changable via MORPH_PERIOD environment\n";
 
-    # Yes, this is "where no records"[sic]
-    break if tds[0].inner_text =~ /There where no records/
+$url_base = "https://da.bundaberg.qld.gov.au/modules/ApplicationMaster/";
+$term_url = "https://da.bundaberg.qld.gov.au/modules/common/Default.aspx?page=disclaimer";
+$da_page  = $url_base . "default.aspx?page=found&1=" .$period. "&4a=333,322,321,324,323,325&6=F";
+$comment_base = "mailto:CEO@bundaberg.qld.gov.au?subject=Development Application Enquiry: ";
 
-    h = tds.map{|td| td.inner_html}
+# Agreed Terms
+$browser = new PGBrowser();
+$page = $browser->get($term_url);
+$form = $page->form();
+if ( empty($form) ) {
+    print "Distil networks kicks in..... :-( \n";
+    exit(0);
+}
+$form->set('ctl00$cphContent$ctl00$btnOk', 'Agree');
+$page = $form->submit();
+sleep(mt_rand(3, 5));
 
-    record = {
-      'info_url' => ("https://da.bundaberg.qld.gov.au/modules/ApplicationMaster/" + tds[0].at('a')['href']).to_s.strip,
-      'comment_url' => comment_url,
-      'council_reference' => clean_whitespace(h[1]),
-      'date_received' => Date.strptime(clean_whitespace(h[2]), '%d/%m/%Y').to_s,
-      'address' => clean_whitespace(tds[3].inner_html.split('<br>').first) + ", QLD",
-      'description' => CGI::unescapeHTML(clean_whitespace(h[3].split('<br>')[1..-1].join)),
-      'date_scraped' => Date.today.to_s
+$page = $browser->get($url_base . 'Default.aspx?page=search');
+sleep(mt_rand(3, 5));
+
+$page = $browser->get($da_page);
+$dom = HtmlDomParser::str_get_html($page->html);
+
+$NumPages = count($dom->find('div[class=rgWrap rgNumPart] a'));
+if ($NumPages === 0) {
+    $NumPages = 1;
+}
+
+for ($i = 1; $i <= $NumPages; $i++) {
+    print "Scraping page $i of $NumPages \n";
+
+    # If more than a single page, fetch the page
+    if ($i > 1) {
+        $form = $page->form();
+        sleep(mt_rand(3, 5));
+        $page = $form->doPostBack($dom->find('div[class=rgWrap rgNumPart] a', $i-1)->href);
+        $dom  = HtmlDomParser::str_get_html($page->html);
     }
 
-    if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
-      ScraperWiki.save_sqlite(['council_reference'], record)
-    else
-      puts "Skipping already saved record " + record['council_reference']
-    end
-  end
-end
+    $results = $dom->find("tr[class=rgRow], tr[class=rgAltRow]");
+    if ( empty($results) ) {
+      print "Distil networks kicks in..... :-( \n";
+      exit(0);
+    }
 
-def scrape_and_follow_next_link(doc, comment_url)
-  scrape_table(doc, comment_url)
-  nextButton = doc.at('.rgPageNext')
-  unless nextButton.nil? || nextButton['onclick'] =~ /return false/
-    form = doc.forms.first
-    
-    # The joy of dealing with ASP.NET
-    form['__EVENTTARGET'] = nextButton['name']
-    form['__EVENTARGUMENT'] = ''
-    # It doesn't seem to work without these stupid values being set.
-    # Would be good to figure out where precisely in the javascript these values are coming from.
-    form['ctl00%24RadScriptManager1']=
-      'ctl00%24cphContent%24ctl00%24ctl00%24cphContent%24ctl00%24Radajaxpanel2Panel%7Cctl00%24cphContent%24ctl00%24ctl00%24RadGrid1%24ctl00%24ctl03%24ctl01%24ctl10'
-    form['ctl00_RadScriptManager1_HiddenField']=
-      '%3B%3BSystem.Web.Extensions%2C%20Version%3D3.5.0.0%2C%20Culture%3Dneutral%2C%20PublicKeyToken%3D31bf3856ad364e35%3Aen-US%3A0d787d5c-3903-4814-ad72-296cea810318%3Aea597d4b%3Ab25378d2%3BTelerik.Web.UI%2C%20Version%3D2009.1.527.35%2C%20Culture%3Dneutral%2C%20PublicKeyToken%3D121fae78165ba3d4%3Aen-US%3A1e3fef00-f492-4ed8-96ce-6371bc241e1c%3A16e4e7cd%3Af7645509%3A24ee1bba%3Ae330518b%3A1e771326%3Ac8618e41%3A4cacbc31%3A8e6f0d33%3Aed16cbdc%3A58366029%3Aaa288e2d'
-    doc = form.submit(form.button_with(:name => nextButton['name']))
-    scrape_and_follow_next_link(doc, comment_url)
-  end
-end
+    # The usual, look for the data set and if needed, save it
+    foreach ($results as $result) {
+        # Slow way to transform the date but it works
+        $date_received = explode(' ', (trim($result->children(2)->plaintext)), 2);
+        $date_received = explode('/', $date_received[0]);
+        $date_received = "$date_received[2]-$date_received[1]-$date_received[0]";
 
-agent = Mechanize.new
+        # Prep a bit more, ready to add these to the array
+        $tempstr = explode('<br/>', $result->children(3)->innertext);
 
-# Jump through bollocks agree screen
-doc = agent.get(starting_url)
-doc = doc.forms.first.submit(doc.forms.first.button_with(:value => "Agree"))
-doc = agent.get(starting_url)
+        # Put all information in an array
+        $record = array (
+            'council_reference' => trim($result->children(1)->plaintext),
+            'address'           => trim(preg_replace('/\s+/', ' ', $tempstr[0]) . ", QLD"),
+            'description'       => preg_replace('/\s+/', ' ', $tempstr[1]),
+            'info_url'          => $url_base . trim($result->find('a',0)->href),
+            'comment_url'       => $comment_base . trim($result->children(1)->plaintext),
+            'date_scraped'      => date('Y-m-d'),
+            'date_received'     => date('Y-m-d', strtotime($date_received))
+        );
 
-scrape_and_follow_next_link(doc, comment_url)
+        # Check if record exist, if not, INSERT, else do nothing
+        $existingRecords = scraperwiki::select("* from data where `council_reference`='" . $record['council_reference'] . "'");
+        if (count($existingRecords) == 0) {
+            print ("Saving record " . $record['council_reference'] . " - " . $record['address']. "\n");
+#            print_r ($record);
+            scraperwiki::save(['council_reference'], $record);
+        } else {
+            print ("Skipping already saved record " . $record['council_reference'] . "\n");
+        }
+    }
+}
+
+?>
